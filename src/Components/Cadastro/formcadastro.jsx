2a509bc8dbcbabd003cd89d833f87dcc7724c6bf
Luisa <luisa.santos042@gmail.com>
2021-09-07 18:16:50 -0300

    reestrutura form
    reestrutura formulario com html semantico

--- src/Components/Cadastro/formcadastro.jsx
+++ src/Components/Cadastro/formcadastro.jsx
@@ -1,31 +1,60 @@
-import React from 'react';
-
 const FormCadastro = () => {
-    return (
-        <>
-
-            <section>
-
-                <h1>Cadastro Usuário</h1>
-
-                <div>
-                    <form action="">
-                        <h1>Nome Completo</h1>
-                        <input type="text" placeholder="Informe seu nome" />
-                        <h1>E-mail</h1>
-                        <input type="text" placeholder="meuemail@fcamara.com.br" />
-                        <h1>Senha</h1>
-                        <input type="password" placeholder="Informe sua senha" />
-                        <h1>Confirmar Senha</h1>
-                        <input type="password" placeholder="Confirme sua senha" />
-                        <button type="submit">Cadastrar</button>
-                    </form>
-                </div>
-
-            </section>
-
-        </>
-    );
+  return (
+    <>
+      <section>
+        <h2>Cadastro de Usuário</h2>
+        <div>
+          <form action=''>
+            <fieldset>
+              {' '}
+              <label for='nome'>Nome completo</label>
+              <input
+                id='nome'
+                type='text'
+                name='nome'
+                placeholder='Informe seu nome'
+              />
+            </fieldset>
+            <fieldset>
+              {' '}
+              <label for='email'>E-mail</label>
+              <input
+                id='email'
+                type='text'
+                name='email'
+                placeholder='meuemail@fcamara.com.br'
+              />
+            </fieldset>
+            <fieldset>
+              {' '}
+              <label for='senha'>Senha</label>
+              <input
+                id='senha'
+                type='password'
+                name='senha'
+                placeholder='Informe sua senha'
+              />
+              <small>
+                Tamanho mínimo de 6 caracteres; utilize ao menos um caractere
+                especial e um número.
+              </small>
+            </fieldset>
+            <fieldset>
+              {' '}
+              <label for='confirmaSenha'>Confirmar Senha</label>
+              <input
+                id='confirmaSenha'
+                type='password'
+                name='senha'
+                placeholder='Confirme sua senha'
+              />
+            </fieldset>
+            <button type='submit'>Cadastrar</button>
+          </form>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default FormCadastro;
