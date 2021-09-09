7504f5e1d027e2daa6423e585aa984c00d313f37
thaysouza <tayanesilvasouzas@gmail.com>
2021-09-09 01:56:45 -0300

    finished styling (saiba-como)

--- src/Components/Home/saiba-como.jsx
+++ src/Components/Home/saiba-como.jsx
@@ -11,33 +11,33 @@ const SaibaComo = () => {
         <>
 
             <section className="area-saiba-como">
-
-                <h1>Saiba como fazer seu agendamento</h1>
-
                 <div>
-                  <div>
+               <h1 className="title-saiba-como">Saiba como fazer seu agendamento</h1>
+
+               <div className="container-content">   
+                  <div className="area-content-icon">
                     <img
                         className="img-icon"
                         src={notebook}
                     />
-                    <p></p>
+                    <p>Faça seu cadastro com seu nome, email e senha e após realizar o login terá acesso para realizar o agendamento</p>
                  </div>
 
-                 <div>
+                 <div className="area-content-icon">
                     <img
                         className="img-icon"
                         src={agenda}
                     />
-                    <p></p>
+                    <p>Você poderá realizar agendamento para qualquer dia que estiver disponível no sistema, conforme o nosso horário de funcionamento</p>
                  </div>
 
 
-                 <div>
+                 <div className="area-content-icon">
                     <img
                         className="img-icon"
                         src={foguete}
                     />
-                    <p></p>
+                    <p>Pronto, agora é só comparecer no dia e horário agendado, esperamos por você em nossos escritórios!</p>
                  </div>
 
 
@@ -48,6 +48,7 @@ const SaibaComo = () => {
                         Cadastre-se
                     </Button>
                 </Link>
+                </div>
             </section>
 
         </>
