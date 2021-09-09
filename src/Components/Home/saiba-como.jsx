886c75aedcfd406a8a0affcc8cbe82a30db4e8a3
thaysouza <tayanesilvasouzas@gmail.com>
2021-09-09 00:07:13 -0300

    adding page elements (saiba-como)

--- src/Components/Home/saiba-como.jsx
+++ src/Components/Home/saiba-como.jsx
@@ -1,14 +1,48 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import Button from './button'
+import "../../styles/saiba-como.css"
+import notebook from '../../img/notebook.png'
+import agenda from '../../img/agenda.png'
+import foguete from '../../img/foguete.png'
 
 const SaibaComo = () => {
     return (
         <>
 
-            <section>
+            <section className="area-saiba-como">
 
                 <h1>Saiba como fazer seu agendamento</h1>
+
+                <div>
+                  <div>
+                    <img
+                        className="img-icon"
+                        src={notebook}
+                    />
+                    <p></p>
+                 </div>
+
+                 <div>
+                    <img
+                        className="img-icon"
+                        src={agenda}
+                    />
+                    <p></p>
+                 </div>
+
+
+                 <div>
+                    <img
+                        className="img-icon"
+                        src={foguete}
+                    />
+                    <p></p>
+                 </div>
+
+
+                    
+                </div>
                 <Link to="/cadastro">
                     <Button>
                         Cadastre-se
