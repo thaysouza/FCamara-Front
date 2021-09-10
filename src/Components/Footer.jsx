08974f8ec06fc9ab74e78a7cc22fe3e0b0174252
Luisa <luisa.santos042@gmail.com>
2021-09-10 20:09:52 -0300

    small fix on footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,70 +1,66 @@
 import React from 'react';
-import logofcamara from "../img/grupo-fcamara-negativo.png";
-import { FiFacebook } from 'react-icons/fi'
-import { FiInstagram } from 'react-icons/fi'
-import { SiGlassdoor } from 'react-icons/si'
-import { FiYoutube } from 'react-icons/fi'
-import { FiLinkedin } from 'react-icons/fi'
+import logofcamara from '../img/grupo-fcamara-negativo.png';
+import { FiFacebook } from 'react-icons/fi';
+import { FiInstagram } from 'react-icons/fi';
+import { SiGlassdoor } from 'react-icons/si';
+import { FiYoutube } from 'react-icons/fi';
+import { FiLinkedin } from 'react-icons/fi';
 
-import "../styles/footer.css"
-import "../styles/global.css"
+import '../styles/footer.css';
 
 const Footer = () => {
-    return (
-        <>
+  return (
+    <>
+      <footer className='footer'>
+        <div className='l-footer'>
+          <h1>
+            <img src={logofcamara} alt='' />
+          </h1>
+        </div>
+        <div className='r-footer'>
+          <a
+            href='https://pt-br.facebook.com/grupo.fcamara/'
+            target='_blank'
+            rel='noreferrer noopener'>
+            {' '}
+            <FiFacebook />
+          </a>
 
-            <footer className="footer">
-                <div className="l-footer">
-                    <h1><img src={logofcamara} /></h1>
-                </div>
-                <div className="r-footer">
-                   <a
-                      href="https://pt-br.facebook.com/grupo.fcamara/"
-                      target="_blank"
-                   >
-                      {" "}
-                    <FiFacebook/>
-                 </a>
+          <a
+            href='https://www.linkedin.com/company/grupofcamara/'
+            target='_blank'
+            rel='noreferrer noopener'>
+            {' '}
+            <FiLinkedin />
+          </a>
 
-                 <a
-                      href="https://www.linkedin.com/company/grupofcamara/"
-                      target="_blank"
-                   >
-                      {" "}
-                    <FiLinkedin/>
-                 </a>
+          <a
+            href='https://www.instagram.com/grupo.fcamara/'
+            target='_blank'
+            rel='noreferrer noopener'>
+            {' '}
+            <FiInstagram />
+          </a>
 
-                 <a
-                      href="https://www.instagram.com/grupo.fcamara/"
-                      target="_blank"
-                   >
-                      {" "}
-                    <FiInstagram/>
-                 </a>
+          <a
+            href='https://www.youtube.com/channel/UCHsQ04xha1YKv48dSzQFlpw'
+            target='_blank'
+            rel='noreferrer noopener'>
+            {' '}
+            <FiYoutube />
+          </a>
 
-
-                 <a
-                      href="https://www.youtube.com/channel/UCHsQ04xha1YKv48dSzQFlpw"
-                      target="_blank"
-                   >
-                      {" "}
-                    <FiYoutube/>
-                 </a>
-
-
-                 <a
-                      href="https://www.glassdoor.com.br/Avalia%C3%A7%C3%B5es/Grupo-FCamara-Avalia%C3%A7%C3%B5es-E668818.htm"
-                      target="_blank"
-                   >
-                      {" "}
-                    <SiGlassdoor/>
-                 </a>      
-            
-                </div>
-            </footer>
-
-        </>
-    );
+          <a
+            href='https://www.glassdoor.com.br/Avalia%C3%A7%C3%B5es/Grupo-FCamara-Avalia%C3%A7%C3%B5es-E668818.htm'
+            target='_blank'
+            rel='noreferrer noopener'>
+            {' '}
+            <SiGlassdoor />
+          </a>
+        </div>
+      </footer>
+    </>
+  );
 };
 
 export default Footer;
