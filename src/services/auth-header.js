507334066de998d713036d41f98618ccd6f6d727
Luisa <luisa.santos042@gmail.com>
2021-09-11 14:52:09 -0300

    adiciona headers para autenticaçao

---
+++ src/services/auth-header.js
@@ -0,0 +1,9 @@
+export default function authHeader() {
+  const user = JSON.parse(localStorage.getItem('usuario'));
+
+  if (user && user.accessToken) {
+    return { 'x-access-token': user.accessToken };
+  } else {
+    return {};
+  }
+}
