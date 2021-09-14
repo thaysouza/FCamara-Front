8f1f546ec52fa0d2efecb178eb2f661f552ab049
Luisa <luisa.santos042@gmail.com>
2021-09-14 20:01:28 -0300

    aplica responsividade a seçao 'saiba como'
    ajusta seçao levando em conta diferentes tamanhos de tela

--- src/Components/Home/saiba-como.jsx
+++ src/Components/Home/saiba-como.jsx
@@ -11,7 +11,7 @@ const SaibaComo = () => {
     <>
       <section id='saibacomo' className='area-saiba-como'>
         <div>
-          <h1 className='title-saiba-como'>Saiba como fazer seu agendamento</h1>
+          <h2 className='title-saiba-como'>Saiba como fazer seu agendamento</h2>
 
           <div className='container-content'>
             <div className='area-content-icon'>
@@ -39,7 +39,7 @@ const SaibaComo = () => {
             </div>
           </div>
           <Link to='/cadastro'>
-            <Button>Cadastre-se</Button>
+            <Button className='btn-cadastro'>Cadastre-se</Button>
           </Link>
         </div>
       </section>
