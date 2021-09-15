07ce87889f198bbbb31f0344e9feb5307318bc5b
thaysouza <tayanesilvasouzas@gmail.com>
2021-09-15 07:34:10 -0300

    manipulating date with react datepicker

--- src/Components/date.jsx
+++ src/Components/date.jsx
@@ -1,29 +1,48 @@
-import React from 'react';
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 import { Link } from 'react-router-dom';
 import '../styles/form.css';
 import '../styles/date.css';
 import Button from './btn_global';
 import agenda from '../img/agenda.png';
 
-const Date = () => {
+const DatePage = () => {
+
+
+    const [selectedDate, setSelectedDate] = useState(null);
 
 
     return (
         <>
             <section className='FormContainer'>
-                <div className='areaContainer btn-large btn-orange'>
+                <div className='areaContainer'>
                     <div>
+
                         <img className="img-date" src={agenda} alt='' />
 
                         <form className="form-date">
                             <h4 className="title-date">Informe o dia de retorno</h4>
 
-                            <input type="date" id="date" name="date" />
-                            <Link to='/success'>
-                                <Button id='cadastroButton' type='submit'>
-                                    Finalizar
-                                </Button>
-                            </Link>
+                            <DatePicker
+                                selected={selectedDate}
+                                onChange={date => setSelectedDate(date)}
+                                dateFormat="dd/MM/yyyy"
+                                minDate={new Date()}
+                                filterDate={date => date.getDay() !== 6 && date.getDay() !== 0}
+                                isClearable
+                                showYearDropdown
+                                scrollableMonthYearDropdown
+                                placeholderText="Please select a date"
+                            />
+                            <div className="btn-large btn-orange">
+                                <Link to='/success'>
+                                    <Button id='cadastroButton' type='submit'>
+                                        Finalizar
+                                    </Button>
+                                </Link>
+                            </div>
                         </form>
                     </div>
                 </div>
@@ -32,4 +51,4 @@ const Date = () => {
     );
 };
 
-export default Date;
+export default DatePage;
